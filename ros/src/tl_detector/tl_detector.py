97efa4e332c87f933813bac3f9b92b24f7a9b5d3
Elgeweily <36533140+Elgeweily@users.noreply.github.com>
2019-01-10 16:40:47 +0400

    Update tl_detector.py

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -72,7 +72,6 @@ class TLDetector(object):
             of the waypoint closest to the red light's stop line to /traffic_waypoint
         Args:
             msg (Image): image from car-mounted camera
-        """ 
         
         rospy.loginfo("new image")
         
@@ -101,6 +100,7 @@ class TLDetector(object):
         self.state_count += 1
         
         rospy.loginfo("end of image cb")
+        """ 
 
     def get_closest_waypoint(self, x, y):
         """Identifies the closest path waypoint to the given position
